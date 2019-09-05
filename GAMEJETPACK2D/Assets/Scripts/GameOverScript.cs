be068c9aa0f4ed59891c7d826f92201e6cd9d869
Sacredouane <54414521+Sacredouane@users.noreply.github.com>
2019-09-05 18:25:43 +0200

    JET

---
+++ GAMEJETPACK2D/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class GameOverScript : MonoBehaviour{
+
+    public float TimeToLoadMenu = 5f;
+
+    private void Start()
+    {
+        StartCoroutine(LoadMenu());
+    }
+ 
+            IEnumerator LoadMenu()
+            {
+        yield return new WaitForSeconds(TimeToLoadMenu);
+        SceneManager.LoadScene("menu");
+            
+    }
+}
