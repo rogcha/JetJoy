ced46d17b5db621255b49815a22d0dbd9cb17e2f
SONY KANA <sonykana2015@gmail.com>
2019-09-06 18:31:18 +0200

    MOD

--- GAMEJETPACK2D/Assets/Scripts/Character.cs
+++ GAMEJETPACK2D/Assets/Scripts/Character.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Character : MonoBehaviour
 {
@@ -92,6 +93,7 @@ public class Character : MonoBehaviour
     {
         if (collision.gameObject.tag == "Obstacle")
         {
+            SceneManager.LoadScene("GAMEOVER");
             isPlaying = false;
             playButton.SetActive(true);
             achButton.SetActive(true);
